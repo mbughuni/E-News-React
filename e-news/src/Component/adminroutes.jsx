b17224d0efec35ef78d678e523cc69e66ec1dcfe
Baljit-Thakur <baljitthakur247@gmail.com>
2025-03-26 15:20:47 +0530

    adminroutes and adminlayout page updated

--- e-news/src/Component/adminroutes.jsx
+++ e-news/src/Component/adminroutes.jsx
@@ -1,5 +1,5 @@
 import { Routes, Route } from "react-router-dom";
-import AdminNavbar from "./adminnavbar";
+import AdminLayout from "./adminlayout"; 
 import AdminDashboard from "./admindashboard";
 import AddedNews from "./AddedNews";
 import AvailableReviews from "./AvailableReviews";
@@ -10,18 +10,17 @@ import AddUsers from "./AddUsers";
 
 const AdminRoutes = () => {
   return (
-    <>
-      <AdminNavbar />
-      <Routes>
-        <Route path="/" element={<AdminDashboard />} />
+    <Routes>
+      <Route path="/" element={<AdminLayout />}>
+        <Route index element={<AdminDashboard />} />
         <Route path="added-news" element={<AddedNews />} />
         <Route path="reviews" element={<AvailableReviews />} />
         <Route path="available-users" element={<AvailableUsers />} />
         <Route path="add-news" element={<AddNews />} />
         <Route path="available-news" element={<AvailableNews />} />
         <Route path="add-user" element={<AddUsers />} />
-      </Routes>
-    </>
+      </Route>
+    </Routes>
   );
 };
 
